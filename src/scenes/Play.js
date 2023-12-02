3c9005e59d847d56917ebb1b6ba68e441f202f93
Yahli <ykijel@ucsc.edu>
2023-12-02 05:03:03 -0800

    temp movement

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,20 +10,32 @@ class Play extends Phaser.Scene {
         
         this.bike = new Bike(this, 25, 25, 'bikeImg').setOrigin(0);
         this.bike.scale = 0.075;
-        this.bike.setGravityY(500);
-        
-        
-        // define keys
-        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.bike.setGravityY(0);
+        this.physics.world.setBoundsCollision(true, true, true, true);
+
+        cursors = this.input.keyboard.createCursorKeys();
 
         
         
     }
 
     update() {
+
+        if(this.bike.y + this.bike.height * this.bike.scale.y / 2 <= this.game.config.height) {
+            this.bike.setGravityY(0);
+        }
+
+        if(cursors.right.isDown)
+        {
+            this.bike.x += 5;
+            //implement screen moving here instead of bike moving
+        }
+        if(cursors.left.isDown)
+        {
+            this.bike.x -= 5;
+            //implement screen moving here instead of bike moving
+        }
+
         
     }
 
